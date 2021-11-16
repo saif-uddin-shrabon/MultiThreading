a74679d0f152c20a59909c85de3729299f3f441e
Saif Uddin Shrabon <61900483+saif-uddin-shrabon@users.noreply.github.com>
2021-11-17 00:49:41 +0600

    first commit

---
+++ src/labaassignment_1/Excl.java
@@ -0,0 +1,15 @@
+
+package labaassignment_1;
+
+
+public class Excl {
+    
+    static void subroutine(){
+        int d =0;
+        int a = 10/d;
+    }
+    public static void main(String[] args) {
+        Excl.subroutine();
+    }
+    
+}
---
+++ src/labaassignment_1/LabAassignment_1.java
@@ -0,0 +1,13 @@
+
+package labaassignment_1;
+
+public class LabAassignment_1 {
+
+   
+    public static void main(String[] args) {
+      int d =0;
+      int a = 42 /d;
+        System.out.println(a);
+    }
+    
+}
